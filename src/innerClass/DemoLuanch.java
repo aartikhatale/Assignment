5d3408e6787102b83c37f21d5eb58f358f994182
aarti <aarti@LAPTOP-VCF4M088>
2020-09-03 20:45:48 +0530

    2nd commit

---
+++ src/innerClass/DemoLuanch.java
@@ -0,0 +1,11 @@
+package innerClass;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoLuanch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", ".\\Software\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
