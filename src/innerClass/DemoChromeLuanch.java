2ff4d665604925ebee5c2a7e80b6c8207bf56010
aarti <aarti@LAPTOP-VCF4M088>
2020-09-04 20:38:25 +0530

    3rd commit

--- src/innerClass/DemoChromeLuanch.java
+++ src/innerClass/DemoChromeLuanch.java
@@ -1,12 +1,15 @@
 package innerClass;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoChromeLuanch {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", ".\\Software\\\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.close();
 	}
 
 }
